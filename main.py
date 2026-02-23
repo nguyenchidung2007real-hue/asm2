94f1c8f07bee96cdc20762b0ff176b000c0fa54e
nguyenchidung2007real-hue <nguyenchidung2007real@gmail.com>
2026-02-23 10:26:50 +0700

    hoan thien

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
         print("1. Xem sản phẩm")
         print("2. Thêm sản phẩm")
         print("3. Tìm sản phẩm")
+        print("4. Xoa san pham")
+        print("5. Cap nhat san pham")
         print("0. Thoát")
         
         chon = input("Chọn: ")
@@ -21,12 +23,17 @@ def main():
             product_manager.them_sp(msp, ten, thuong_hieu, gia, sl)
         elif chon == "3":
             ma = input("Nhập mã cần tìm: ")
-            ket_qua = product_manager.tim_theo_id(ma)
+            ket_qua = product_manager.search_product_by_name(ma)
             if ket_qua :
                 print(f"Tìm thấy: {ket_qua}")
             else:
                 print("Không tìm thấy")
+        elif chon =="4":
+            ma = input("Nhập mã cần xoa: ")
+            x= product_manager.delete_product(ma)
+        elif chon=="5":
+            ma = input("Nhập mã cần cap nhat: ")
+            x=product_manager.update_product(ma)
         elif chon == "0":
             break
-
 main()
